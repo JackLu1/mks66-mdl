fb5aba6ba48e2cc643b8c3ec0f29ec0122dcbffb
Jack Lu <jlu9@stuy.edu>
2019-05-14 15:37:57 -0400

    work

--- script.py
+++ script.py
@@ -50,6 +50,42 @@ def run(filename):
     #for s in symbols:
     #    print s
     #    print symbols[s]
-    #print symbols
+    print symbols
     for command in commands:
         print command
+        op = command['op']
+
+        if op == 'push':
+            stack.append( [x[:] for x in stack[-1]] )
+
+        elif op == 'pop':
+            stack.pop()
+
+        elif op == 'scale':
+            args = command['args']
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            if command['knob']:
+                knob = command['knob']
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif op == 'move':
+            args = command['args']
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif op == 'rotate':
+            #print 'ROTATE\t' + str(args)
+            args = command['args']
+            theta = float(args[1]) * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+
